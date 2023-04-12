1887886d25411844f8eb05708108d912a3f102a9
cj <carocean.jofers@icloud.com>
2023-04-13 00:20:40 +0800

    Changes

--- src/main/java/cj/geochat/server/eureka/CjGeochatEurekaServerApplication.java
+++ src/main/java/cj/geochat/server/eureka/CjGeochatEurekaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication(scanBasePackages = "cj.geochat.eureka.server")
+@SpringBootApplication(scanBasePackages = "cj.geochat.server.eureka")
 public class CjGeochatEurekaServerApplication {
 
     public static void main(String[] args) {
